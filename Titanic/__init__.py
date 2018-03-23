4645c4a8505677b07416ab4fe430750d833604e2
mickey0524 <buptbh@163.com>
2018-03-24 01:20:09 +0800

    feat(Titanic): 初始化

---
+++ Titanic/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+
+data_train = pd.read_csv('./train.csv')
+data_test = pd.read_csv('./test.csv')
---
+++ Titanic/titanic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# author: mickey0524
+# first kaggle competition
+# 2017-03-24 
+
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+
